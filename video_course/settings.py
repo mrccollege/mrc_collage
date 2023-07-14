edad53ac3d3ca7bb26fa015677ad4651a282cfff
mrc_college <mrctherapy2023@gmail.com>
2023-07-14 12:36:55 +0530

    setting file

--- video_course/settings.py
+++ video_course/settings.py
@@ -119,6 +119,8 @@ STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Maximum file size for chunked upload (in bytes)
+CHUNKED_UPLOAD_MAX_MEMORY_SIZE = 524288000  # 500MB
 
 CHUNK_SIZE = 8192  # or a larger value
 
