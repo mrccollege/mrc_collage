e4c30435e67748fb5002c9dae142fbd2ec81345c
mrc_college <mrctherapy2023@gmail.com>
2023-08-27 10:55:19 +0530

    'hello'

--- courses/models.py
+++ courses/models.py
@@ -8,7 +8,15 @@ class FileType(models.Model):
     def __str__(self):
         return self.file_type
 
+class CourseMaster(models.Model):
+    name = models.CharField(max_length=1000, null=True)
+    description = models.TextField(null=True)
+    image = models.ImageField(upload_to='course_images/', null=True, default='', blank=True)
+    def __str__(self):
+        return
+
 class Course(models.Model):
+    course_master = models.ForeignKey(CourseMaster, on_delete=models.PROTECT, default=1)
     name = models.CharField(max_length=1000, null=True)
     description = models.TextField(null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
