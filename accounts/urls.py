6e6449ad7681d0392658af0a80fe4042c95e4867
mrc_college <mrctherapy2023@gmail.com>
2023-09-25 22:25:37 +0530

    'l'

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('login/', views.login_account, name='login_account'),
     path('logout/', views.logout_account, name='logout_account'),
 
+    path('term_condition/', views.term_condition, name='term_condition'),
+    path('cancel_refund/', views.cancel_refund, name='cancel_refund'),
+    path('shiping/', views.shiping, name='shiping'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
+
 ]
