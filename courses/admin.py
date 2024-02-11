16d043c9eaac073f789885046ecdb3c9b679a0d9
mrc_college <mrctherapy2023@gmail.com>
2024-02-11 11:22:00 +0530

    p

--- courses/admin.py
+++ courses/admin.py
@@ -20,9 +20,9 @@ class MonthMoneyAdmin(admin.ModelAdmin):
 
 
 class CoursePurchasedAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'course', 'payment_status', 'price', 'coupon_code', 'discount', 'totalprice', 'month', 'start_date', 'end_date')
-    list_filter = ('id', 'user', 'course', 'payment_status', 'price', 'coupon_code', 'discount', 'totalprice', 'month', 'start_date', 'end_date')
-    search_fields = ('id', 'user', 'course', 'razorpay_order_id', 'payment_status')
+    list_display = ('user', 'course', 'razorpay_order_id', 'payment_status', 'coupon_code', 'month', 'start_date', 'end_date')
+    list_filter = ('user', 'course', 'razorpay_order_id', 'payment_status', 'coupon_code', 'month', 'start_date', 'end_date')
+    search_fields = ('user', 'course', 'razorpay_order_id', 'payment_status')
 
 
 # Register your models here.
