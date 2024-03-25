8a72a1826c5f87dc3ed08240febe6e42adb6ecd0
mrc_college <mrctherapy2023@gmail.com>
2024-03-25 12:20:44 +0530

    p

--- video_course/settings.py
+++ video_course/settings.py
@@ -69,23 +69,23 @@ WSGI_APPLICATION = 'video_course.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mrc_college',
-        'USER': 'mrc_college',
-        'PASSWORD': 'Hello123456#',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mrc_college',
+#         'USER': 'mrc_college',
+#         'PASSWORD': 'Hello123456#',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
