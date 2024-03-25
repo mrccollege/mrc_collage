f5ea18b717f5b308310dcc9669efc91b55ac78b2
mrc_college <mrctherapy2023@gmail.com>
2024-03-25 11:14:37 +0530

    p

--- demo/admin.py
+++ demo/admin.py
@@ -1,11 +1,2 @@
 from django.contrib import admin
-from .models import DemoCourseCategory, DemoCourse, DemoClass, UserDemoCourse, UserDemoClass
 
-# Register your models here.
-
-admin.site.register(DemoCourseCategory)
-admin.site.register(DemoCourse)
-admin.site.register(DemoClass)
-
-admin.site.register(UserDemoCourse)
-admin.site.register(UserDemoClass)
