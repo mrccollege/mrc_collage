b888cefe979f63b6c3a2d1370d30b06f84475a47
sanjay <sanjaypython22@gmail.com>
2025-07-01 11:29:51 +0530

    p

--- courses/models.py
+++ courses/models.py
@@ -40,6 +40,7 @@ class Course(models.Model):
     description = models.TextField(null=True, blank=True)
     discount = models.CharField(max_length=10, default=0, blank=True, null=True)
     course_image = models.ImageField(upload_to='course_images/', null=True, blank=True)
+    coupon_image = models.ImageField(upload_to='coupon_images/', null=True, blank=True)
     demo_video = models.FileField(upload_to='course_demo', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(null=True, blank=True)
