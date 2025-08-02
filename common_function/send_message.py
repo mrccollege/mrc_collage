ae69f2b8737b7285a03961c0af046dac29dcc6c9
sanjay <sanjaypython22@gmail.com>
2025-08-02 14:12:18 +0530

    p

--- common_function/send_message.py
+++ common_function/send_message.py
@@ -2,6 +2,7 @@ import requests
 
 
 def send_sms(mobile, message):
+    url = "http://msg.msgclub.net/rest/services/sendSMS/sendGroupSms"
     params = {
         'AUTH_KEY': '3380567192fd2e6d18f63985aace',
         'message': message,
@@ -12,13 +13,13 @@ def send_sms(mobile, message):
     }
 
     response = requests.get(url, params=params)
-    if response.status_code == 200:
-        data = response.json()  # Parse the JSON response to a Python dict
-        response_code = data.get('responseCode')
-        response_value = data.get('response')
-
-        # Now you can use the values
-        print("Response Code:", response_code)
-        print("Response:", response_value)
-    else:
-        print("Failed to send SMS", response.text)
+    # if response.status_code == 200:
+    #     data = response.json()  # Parse the JSON response to a Python dict
+    #     response_code = data.get('responseCode')
+    #     response_value = data.get('response')
+    #
+    #     # Now you can use the values
+    #     print("Response Code:", response_code)
+    #     print("Response:", response_value)
+    # else:
+    #     print("Failed to send SMS", response.text)
