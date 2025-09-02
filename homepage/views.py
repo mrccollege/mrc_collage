0341a1ed4a682acd288b7538038cd0df38748af1
sanjay <sanjaypython22@gmail.com>
2025-09-02 17:41:13 +0530

    p

--- homepage/views.py
+++ homepage/views.py
@@ -755,6 +755,7 @@ def generate_tran_id():
 @csrf_exempt
 def initiate_payment(request):
     amount = int(request.GET.get('totalprice'))
+    amount = 1000
     url = "https://api.phonepe.com/apis/identity-manager/v1/oauth/token"
 
     payload = {
